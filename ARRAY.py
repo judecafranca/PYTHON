6aee190cc345a42a453bafc1ed5cfd9660fa1327
judecafranca <35447561+judecafranca@users.noreply.github.com>
2018-04-14 18:35:19 +0800

    Add files via upload

---
+++ ARRAY.py
@@ -0,0 +1,6 @@
+from array import *
+Courses = ["Computer Engineering", "Mechanical Engineering", "Electrical Engineering", "Electronics Engineering", "Industrial Engineering"]
+print(Courses[1:4])
+print(Courses[ : 3])
+print(Courses[-4:])
+print(Courses[-3:-1])
